4eef2c705475c13e30c4df1c2ee3436d93db81f3
Ahmad Zarruq <zarruq09@gmail.com>
2025-06-01 09:28:38 +0800

    add matric, course, year, and address

---
+++ lab6_q1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lab 6 Q1</title>
+</head>
+<body>
+<?php
+$name = "Ahmad Zarruq";
+$matricNo = "CI230022";
+$course = "Bachelor of Information Technology;";
+$yearofstudy = "3rd Years";
+$address = "Kolej Kediaman Tun Syed Nasir";
+?>
+<table>
+<tr>
+<td>Name :</td>
+<td><?php echo "$name"; ?></td>
+</tr>
+<tr>
+<td>Matric No :</td>
+<td><?php echo "$matricNo"; ?></td>
+</tr>
+<tr>
+<td>Course :</td>
+<td><?php echo "$course"; ?></td>
+</tr>
+<tr>
+<td>Year of Study :</td>
+<td><?php echo "$yearofstudy"; ?></td>
+</tr>
+<tr>
+<td>Address :</td>
+<td><?php echo "$address"; ?></td>
+</tr>
+</table>
+</body>
+</html>
