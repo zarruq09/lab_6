032568e80a3c5fb3b6ff6f3f0ee8ab10e1a69c1f
Ahmad Zarruq <zarruq09@gmail.com>
2025-06-01 09:33:27 +0800

    copy coding from pdf

---
+++ lab6_q2.php
@@ -0,0 +1,17 @@
+$student = [
+    [
+        'name' => 'Alice',
+        'program' => 'BIP',
+        'age' => 20,
+    ],
+    [
+        'name' => 'Bob',
+        'program' => 'BIS',
+        'age' => 22,
+    ],
+    [
+        'name' => 'Raju',
+        'program' => 'BIT',
+        'age' => 19,
+    ],
+];
